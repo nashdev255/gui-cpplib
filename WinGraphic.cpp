34a286ba80b4ee0d40a0cc24c92678a99d21b1d4
nashdev255 <yutori1107@gmail.com>
2023-07-08 14:39:53 +0900

    feat : WinGraphic.hpp, WinGraphic.cpp

---
+++ WinGraphic.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file WinGraphic.cpp
+ * @version 0.0.1
+ * @author Copyright (C) 2023 by nashdev255
+ * @brief handmade library for graphics of cpp 
+ * 
+*/
+
+#include "WinGraphic.hpp"
---
+++ WinGraphic.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file WinGraphic.hpp
+ * @version 0.0.1
+ * @author Copyright (C) 2023 by nashdev255
+ * @brief handmade library for graphics of cpp 
+ * 
+*/
+
+#include <windows.h>
+#include <string>
