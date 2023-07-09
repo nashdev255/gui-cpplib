39ca389023e56c96526ebbc9e1ac221ed902f1ad
nashdev255 <yutori1107@gmail.com>
2023-07-09 09:17:53 +0900

    improvement : define to constexpr

--- WinGraphic.cpp
+++ WinGraphic.cpp
@@ -10,6 +10,16 @@
 #include <windows.h>
 #include <string>
 
+/**
+ * print a character
+ * 
+ * @param ch character
+ * @note typedef void *HANDLE
+ * @note typedef unsigned long DWORD
+ * @note 
+*/
 void cout32_f(char32_t ch) {
     HANDLE hCons = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD len = 1;
+    WriteConsoleW(hCons, &ch, len, &len, 0);
 }
